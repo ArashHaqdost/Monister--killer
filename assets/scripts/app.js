660457a4a7b46d0daee93fdd9e6bd7a65b246bc1
Arash <shokrullahhaqdost87@gmail.com>
2022-04-10 23:59:58 +0500

    added the loginc oprator in if statment

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -14,10 +14,12 @@ function attackHandler(){
   const playerDamage= dealPlayerDamage(MONISTER_ATTACK_VALUE);
   currentPlayerHealth -=playerDamage;
 
-  if (currentMonsterHealth <= 0){
+  if (currentMonsterHealth <= 0 && currentPlayerHealth >0){
     alert('you won ');
-  }else if(currentPlayerHealth<=0){
+  }else if(currentPlayerHealth<=0 && currentMonsterHealth >0){
     alert('you lost');
+  }else if (currentMonsterHealth<=0 && currentPlayerHealth<=0){
+    alert('you have a drow');
   }
 }
 
