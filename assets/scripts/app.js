93eefd0dedbdfc0bcc212122be5a5c0bf131e89e
Arash <shokrullahhaqdost87@gmail.com>
2022-04-10 23:39:45 +0500

    added the if conditions in attack function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const  ATTACK_VALUE=10;
+const MONISTER_ATTACK_VALUE=14;
 
 let chosenMaxLife=100;
 let currentMonsterHealth=chosenMaxLife;
@@ -10,6 +11,14 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler(){
   const damage =dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -=damage;
+  const playerDamage= dealPlayerDamage(MONISTER_ATTACK_VALUE);
+  currentPlayerHealth -=playerDamage;
+
+  if (currentMonsterHealth <= 0){
+    alert('you won ');
+  }else if(currentPlayerHealth<=0){
+    alert('you lost');
+  }
 }
 
 attackBtn.addEventListener('click',attackHandler);
