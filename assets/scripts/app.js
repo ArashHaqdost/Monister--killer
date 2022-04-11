f393b83bbd74f0b9bdc253b25359f01c398c5695
Arash <shokrullahhaqdost87@gmail.com>
2022-04-11 06:38:27 +0500

    added function attackMonster mode

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,26 +1,41 @@
-const  ATTACK_VALUE=10;
-const MONISTER_ATTACK_VALUE=14;
+const STRONG_ATTACK_VALUE = 17;
+const ATTACK_VALUE = 10;
+const MONISTER_ATTACK_VALUE = 14;
 
-let chosenMaxLife=100;
-let currentMonsterHealth=chosenMaxLife;
-let currentPlayerHealth=chosenMaxLife;
-
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler(){
-  const damage =dealMonsterDamage(ATTACK_VALUE);
-  currentMonsterHealth -=damage;
-  const playerDamage= dealPlayerDamage(MONISTER_ATTACK_VALUE);
-  currentPlayerHealth -=playerDamage;
+//satrt functon attackMonster mode
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONISTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
 
-  if (currentMonsterHealth <= 0 && currentPlayerHealth >0){
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('you won ');
-  }else if(currentPlayerHealth<=0 && currentMonsterHealth >0){
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert('you lost');
-  }else if (currentMonsterHealth<=0 && currentPlayerHealth<=0){
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
     alert('you have a drow');
   }
 }
-
-attackBtn.addEventListener('click',attackHandler);
+//start strong attack handlaer
+function strongAttackHandler() {
+  attackMonster('STRONG_ATTACK');
+}
+//START ATTACK HANDLER
+function attackHandler() {
+  attackMonster('ATTACK');
+}
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
