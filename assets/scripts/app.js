20c53227c60fd755378f454389f6cf4fc13c9c0f
Arash <shokrullahhaqdost87@gmail.com>
2022-04-12 22:35:46 +0500

    added some part of swich csae

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,71 +2,70 @@ const STRONG_ATTACK_VALUE = 17;
 const ATTACK_VALUE = 10;
 const MONISTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
-const MODE_ATTACK='ATTACK';//mode_attack =0;
-const MODE_STRONG_ATTACK="STRONG_ATTACK"; //or may 1;
-const LOG_EVENT_PLAYER_ATTACK='PLAYER_ATTACK';
-const LOG_EVENT_PLAYER_STRONG_ATTACK='STRONG_ATTACK';
-const LOG_EVENT_MONSTER_ATTACK='MONSTER_ATTACK';
-const LOG_EVENT_PLAYER_HEAL='PLAYER_HEAL';
-const LOG_EVENT_GAME_OVER='GAME_OVER';
+const MODE_ATTACK = 'ATTACK'; //mode_attack =0;
+const MODE_STRONG_ATTACK = 'STRONG_ATTACK'; //or may 1;
+const LOG_EVENT_PLAYER_ATTACK = 'PLAYER_ATTACK';
+const LOG_EVENT_PLAYER_STRONG_ATTACK = 'STRONG_ATTACK';
+const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
+const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
+const LOG_EVENT_GAME_OVER = 'GAME_OVER';
 const enteredValidInput = parseInt(
   prompt('enter your life and monister life here :', '')
 );
 
 let chosenMaxLife = parseInt(enteredValidInput);
-if (isNaN(chosenMaxLife) || chosenMaxLife <= 0 ) {
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
   chosenMaxLife = 100;
 }
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
-let battleLog=[];
+let battleLog = [];
 
 adjustHealthBars(chosenMaxLife);
 //wirte toLog fuction
-function writeToLog(ev,val,monsterHealth,playerHealth){
-  let logEntry={
-    Event:ev,
-    value:val,
-    finalMonsterHealth:monsterHealth,
-    finalPlayerHealth:playerHealth
+function writeToLog(ev, val, monsterHealth, playerHealth) {
+  let logEntry = {
+    Event: ev,
+    value: val,
+    finalMonsterHealth: monsterHealth,
+    finalPlayerHealth: playerHealth,
+  };
+  if (ev === LOG_EVENT_PLAYER_ATTACK) {
+    logEntry.target = 'MONSTER';
+  } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+    logEntry = {
+      Event: ev,
+      value: val,
+      target: 'MONSTER',
+      finalMonaterHealth: monsterHealth,
+      finalPlayerHealth: playerHealth,
+    };
+  } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
+    logEntry = {
+      Event: ev,
+      value: val,
+      target: 'PLYER',
+      finalMonaterHealth: monsterHealth,
+      finalPlayerHealth: playerHealth,
+    };
+  } else if (ev === LOG_EVENT_PLAYER_HEAL) {
+    logEntry = {
+      Event: ev,
+      value: val,
+      target: 'PLAYER',
+      finalMonaterHealth: monsterHealth,
+      finalPlayerHealth: playerHealth,
+    };
+  } else if (ev === LOG_EVENT_GAME_OVER) {
+    logEntry = {
+      Event: ev,
+      value: val,
+      finalMonaterHealth: monsterHealth,
+      finalPlayerHealth: playerHealth,
+    };
   }
-  if (ev===LOG_EVENT_PLAYER_ATTACK){
-    logEntry.target='MONSTER';
-}else if(ev===LOG_EVENT_PLAYER_STRONG_ATTACK){
-  logEntry={
-    Event:ev,
-    value:val,
-    target :'MONSTER',
-    finalMonaterHealth:monsterHealth,
-    finalPlayerHealth:playerHealth
-  };
-}else if(ev===LOG_EVENT_MONSTER_ATTACK){
-  logEntry={
-    Event:ev,
-    value:val,
-    target:'PLYER',
-    finalMonaterHealth:monsterHealth,
-    finalPlayerHealth:playerHealth
-  };
-}else if(ev===LOG_EVENT_PLAYER_HEAL){
-  logEntry={
-    Event:ev,
-    value:val,
-    target:'PLAYER',
-    finalMonaterHealth:monsterHealth,
-    finalPlayerHealth:playerHealth
-  };
-}else if(ev===LOG_EVENT_GAME_OVER){
-
-  logEntry={
-    Event:ev,
-    value:val,
-    finalMonaterHealth:monsterHealth,
-    finalPlayerHealth: playerHealth
-  };
-}
-battleLog.push(logEntry);
+  battleLog.push(logEntry);
 }
 
 //  start rest function
@@ -80,7 +79,12 @@ function endRound() {
   const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONISTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
-  writeToLog(LOG_EVENT_MONSTER_ATTACK,playerDamage,currentMonsterHealth,currentPlayerHealth);
+  writeToLog(
+    LOG_EVENT_MONSTER_ATTACK,
+    playerDamage,
+    currentMonsterHealth,
+    currentPlayerHealth
+  );
   if (currentPlayerHealth <= 0 && hasBonusLife) {
     hasBonusLife = false;
     removeBonusLife();
@@ -91,16 +95,28 @@ function endRound() {
 
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('you won ');
-  writeToLog(LOG_EVENT_GAME_OVER,'PLYER WON',currentMonsterHealth,currentPlayerHealth);
-
+    writeToLog(
+      LOG_EVENT_GAME_OVER,
+      'PLYER WON',
+      currentMonsterHealth,
+      currentPlayerHealth
+    );
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert('you lost');
-  writeToLog(LOG_EVENT_GAME_OVER,'MONSTER WON',currentMonsterHealth,currentPlayerHealth);
-
+    writeToLog(
+      LOG_EVENT_GAME_OVER,
+      'MONSTER WON',
+      currentMonsterHealth,
+      currentPlayerHealth
+    );
   } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
     alert('you have a drow');
-  writeToLog(LOG_EVENT_GAME_OVER,'A DRAW',currentMonsterHealth,currentPlayerHealth);
-
+    writeToLog(
+      LOG_EVENT_GAME_OVER,
+      'A DRAW',
+      currentMonsterHealth,
+      currentPlayerHealth
+    );
   }
   if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
     rest();
@@ -112,14 +128,14 @@ function attackMonster(mode) {
   let logEvent;
   if (mode === MODE_ATTACK) {
     maxDamage = ATTACK_VALUE;
-    logEvent=LOG_EVENT_PLAYER_ATTACK;
+    logEvent = LOG_EVENT_PLAYER_ATTACK;
   } else if (mode === MODE_STRONG_ATTACK) {
     maxDamage = STRONG_ATTACK_VALUE;
-    logEvent=LOG_EVENT_PLAYER_STRONG_ATTACK;
+    logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK;
   }
   const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
-  writeToLog(logEvent,damage,currentMonsterHealth,currentPlayerHealth);
+  writeToLog(logEvent, damage, currentMonsterHealth, currentPlayerHealth);
 
   endRound();
 }
@@ -147,27 +163,76 @@ function healPlayerHandler() {
   }
   increasePlayerHealth(HEAL_VALUE);
   currentPlayerHealth += HEAL_VALUE;
-  writeToLog(LOG_EVENT_PLAYER_HEAL,healValue,currentMonsterHealth,currentPlayerHealth);
+  writeToLog(
+    LOG_EVENT_PLAYER_HEAL,
+    healValue,
+    currentMonsterHealth,
+    currentPlayerHealth
+  );
 
   endRound();
 }
 //fucntion print log handlar
-function printLogHandler(){
+function printLogHandler() {
   console.log(battleLog);
 }
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
 healBtn.addEventListener('click', healPlayerHandler);
-logBtn.addEventListener('click',printLogHandler)
+logBtn.addEventListener('click', printLogHandler);
 
 // this is the logic and compertaive  operators
 
-const userName="ARash";
-const altName="";
-console.log(userName==="ARash");//genrate and print a boolean thats true
+const userName = 'ARash';
+const altName = '';
+console.log(userName === 'ARash'); //genrate and print a boolean thats true
 console.log(userName);
-console.log(userName|| null)//
-console.log(userName||"ARash");
+console.log(userName || null); //
+console.log(userName || 'ARash');
 console.log(userName && 'Anna'); // userName is truthy, hence second (!) value is returned => 'Anna'
 console.log(altName && 'Anna'); // altName is falsy, hence first value is returned => ''
-console.log(userName && '');
+// console.log(userName && '');
+
+// let age= prompt('Enter your Age');
+// switch(true){
+//     case age < 13 :
+//         alert("you can play this game ");
+//         break;
+//     case age >= 13 :
+//         alert("you cant play this game ");
+//         break;
+//   }
+
+// let enterAge = prompt('Enter your age');
+// let morThan13 = parseInt(enterAge) > 13;
+// let lesthan13 = parseInt(enterAge) <= 13;
+
+// switch(morThan13 ||lesthan13){
+//   case morThan13:
+//       alert("you can not play this game");
+//       break;
+//   case lesthan13:
+//     alert("you can play this game");
+//     break;
+// }
+let age = prompt("enter your age");
+
+switch (age){
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  case 7:
+  case 8:
+  case 9:
+  case 10:
+  case 12:
+  case 13:
+     alert("you can play this game");
+     break;
+  default :
+     alert("you can not paly this game ");
+     break;
+}
